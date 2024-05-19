b11d8f301ad9e315e4fee72e6c14d1dfd04d0cc3
Kirill Toptun <kirilltoptun@gmail.com>
2024-05-19 15:17:05 +1200

    first commit

---
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.calculatorapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
