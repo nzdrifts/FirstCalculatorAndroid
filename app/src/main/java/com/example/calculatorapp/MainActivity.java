f11afea20d6afa55d45300db13627e2b46ebe532
Kirill Toptun <kirilltoptun@gmail.com>
2024-05-20 23:17:21 +1200

    added all operations and btnMEM, btnSignChange

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -10,14 +10,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.calculatorapp.models.EvaluateOperations;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnClear, btnPlus, btnEquals, btnMul, btnDiv, btnMinus, btnErase;
+    private Button btnClear, btnPlus, btnEquals, btnMul, btnDiv, btnMinus, btnErase, btnDecimal, btnMEM, btnSignChange;
     private List<Button> btnList;
-    private TextView tvValue;
+    private TextView tvValue,tvLastOperation;
 
 
     @Override
@@ -31,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void clickListeners(){
         // sets text to "" when btnClear is pressed
-        this.btnClear.setOnClickListener(v -> this.tvValue.setText(""));
+        this.btnClear.setOnClickListener(v -> {
+            this.tvValue.setText("");
+            String lo = "Last Operation: ";
+            this.tvLastOperation.setText(lo);
+        });
 
         // btnPlus
         this.btnPlus.setOnClickListener(v -> appendOperation(" + "));
@@ -41,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
         this.btnMul.setOnClickListener(v -> appendOperation(" x "));
         // btnDiv
         this.btnDiv.setOnClickListener(v -> appendOperation(" / "));
+
+        // btnDecimal
+        this.btnDecimal.setOnClickListener(v -> this.tvValue.append("."));
 
         this.btnErase.setOnClickListener(v -> {
             String s = this.tvValue.getText().toString();
@@ -58,10 +66,46 @@ public class MainActivity extends AppCompatActivity {
 
         // equals button
         this.btnEquals.setOnClickListener((v)->{
+            String txtValue = this.tvValue.getText().toString();
+            // remove operation at end if it is present
+            if(txtValue.endsWith(" ")) txtValue=txtValue.substring(0,txtValue.length()-3);
+            // append last_operation
+            String s = "Last Operation: ";
+            this.tvLastOperation.setText(s.concat(txtValue));
             // evaluated the value of operations currently displayed
-            long result = EvaluateOperations.evaluate(this.tvValue.getText().toString());
+            double result = EvaluateOperations.evaluatePriority(txtValue);
             // set text to result
             this.tvValue.setText(String.valueOf(result));
+        });
+
+        // MEM button
+        this.btnMEM.setOnClickListener((v -> {
+            String memText = this.tvLastOperation.getText().toString();
+            this.tvValue.setText(memText.substring(16));
+            String lo = "Last Operation: ";
+            this.tvLastOperation.setText(lo);
+        }));
+
+        // sign change button
+        this.btnSignChange.setOnClickListener(v -> {
+            // puts tvValue into a list
+            String[] list = this.tvValue.getText().toString().split(" ");
+            if(this.tvValue.getText().toString().isEmpty()) return;
+            int listSize = list.length-1;
+            if(list[listSize].equals("+") || list[listSize].equals("-") || list[listSize].equals("/") || list[listSize].equals("x")){
+                double newVal = Double.parseDouble(list[listSize-1]) * -1;
+                list[listSize-1] = Double.toString(newVal);
+            }
+            else{
+                double newVal = Double.parseDouble(list[listSize]) * -1;
+                list[listSize] = Double.toString(newVal);
+            }
+            if(list[listSize].equals("+") || list[listSize].equals("-") || list[listSize].equals("/") || list[listSize].equals("x")){
+                String returnString = String.join(" ", Arrays.asList(list)) + " ";
+                this.tvValue.setText(returnString);
+            }else{
+                this.tvValue.setText( String.join(" ", Arrays.asList(list)));
+            }
         });
     }
 
@@ -81,6 +125,9 @@ public class MainActivity extends AppCompatActivity {
         this.btnMul = findViewById(R.id.button_multiply);
         this.btnDiv = findViewById(R.id.button_divide);
         this.btnErase = findViewById(R.id.button_erase);
+        this.btnDecimal = findViewById(R.id.button_decimal);
+        this.btnMEM = findViewById(R.id.button_MEM);
+        this.btnSignChange = findViewById(R.id.button_change_sign);
 
         // populate btnList array with buttons, index = button number
         this.btnList = new ArrayList<>();
@@ -96,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
         this.btnList.add(findViewById(R.id.button_9));
 
         this.tvValue = findViewById(R.id.textView);
+        this.tvLastOperation = findViewById(R.id.last_operation);
     }
 
 }
--- app/src/main/java/com/example/calculatorapp/models/EvaluateOperations.java
+++ app/src/main/java/com/example/calculatorapp/models/EvaluateOperations.java
@@ -1,13 +1,46 @@
 package com.example.calculatorapp.models;
 
+import java.util.Arrays;
+
 public class EvaluateOperations {
 
-    public static long evaluate(String txtValue) { // e.g. 1 + 2 + 99
-        String[] list = txtValue.split(" \\+ ");
-        long result = 0;
-        for(String s : list){
-            result += Long.parseLong(s);
+    public static double evaluate(String txtValue) { // e.g. 1 + 2 - 99 (+ and -)
+        if (txtValue.isEmpty()) return 0;
+
+        String[] list = txtValue.split(" ");
+        double result = Double.parseDouble(list[0]);
+
+        for (int i = 1; i < list.length-1; i++) {
+            if (list[i].equals("+")){
+                result += Double.parseDouble(list[i+1]);
+            }
+            if (list[i].equals("-")){
+                result -= Double.parseDouble(list[i+1]);
+            }
         }
         return result;
+    }
+
+    public static double evaluatePriority(String txtValue) { //return a string with no / or x
+        String[] list = txtValue.split(" ");
+        for (int i = 1; i < list.length-1; i++) {
+            if (list[i].equals("/")){
+                String result = joinList(list,0,i-1) + " " + (Double.parseDouble(list[i-1])/Double.parseDouble(list[i+1])) + " " + joinList(list,i+1,list.length-1);
+                if(result.startsWith(" ")) result = result.substring(1);
+                if(result.endsWith(" ")) result = result.substring(0, result.length()-1);
+                return evaluatePriority(result);
+            }
+            if (list[i].equals("x")){
+                String result = joinList(list,0,i-1) + " " + (Double.parseDouble(list[i-1])*Double.parseDouble(list[i+1])) + " " + joinList(list,i+1,list.length-1);
+                if(result.startsWith(" ")) result = result.substring(1);
+                if(result.endsWith(" ")) result = result.substring(0, result.length()-1);
+                return evaluatePriority(result);
+            }
+        }
+        return evaluate(txtValue);
+    }
+
+    private static String joinList(String[] list, int start, int end){
+        return String.join(" ", Arrays.asList(list).subList(start,end));
     }
 }
